364bde6d45a6c7bd9dba0158295ad739b9a49318
Yusuke Ohi <yohi.consadole12@gmail.com>
2024-03-08 01:05:39 +0900

    更新

--- bitbucket_pr_review.py
+++ bitbucket_pr_review.py
@@ -11,8 +11,8 @@ import os
 import logging
 from dotenv import load_dotenv
 from litellm import completion
-from prompt import CodeRabbitPrompt
 from pprint import pprint
+import prompt as prompt_module
 
 
 dir_path = os.path.dirname(os.path.abspath("__file__"))
@@ -21,40 +21,65 @@ load_dotenv(dotenv_path, verbose=True)
 
 AI_MODEL = 'gemini/gemini-pro'
 
-prompt = CodeRabbitPrompt()
+prompt = prompt_module.MyPrompt()
 
-level = logging.ERROR
+# level = logging.ERROR
 
-# --------------------------------
-# 1.loggerの設定
-# --------------------------------
-# loggerオブジェクトの宣言
-logger = logging.getLogger(__name__)
+# # --------------------------------
+# # 1.loggerの設定
+# # --------------------------------
+# # loggerオブジェクトの宣言
+# logger = logging.getLogger(__name__)
+# 
+# # loggerのログレベル設定(ハンドラに渡すエラーメッセージのレベル)
+# logger.setLevel(level)
+# 
+# # --------------------------------
+# # 2.handlerの設定
+# # --------------------------------
+# # handlerの生成
+# stream_handler = logging.StreamHandler()
+# 
+# # handlerのログレベル設定(ハンドラが出力するエラーメッセージのレベル)
+# stream_handler.setLevel(level)
+# 
+# # ログ出力フォーマット設定
+# handler_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # noqa: E501
+# stream_handler.setFormatter(handler_format)
+# 
+# # --------------------------------
+# # 3.loggerにhandlerをセット
+# # --------------------------------
+# logger.addHandler(stream_handler)
+# 
+# # --------------------------------
+# # ログ出力テスト
+# # --------------------------------
 
-# loggerのログレベル設定(ハンドラに渡すエラーメッセージのレベル)
-logger.setLevel(level)
-
-# --------------------------------
-# 2.handlerの設定
-# --------------------------------
-# handlerの生成
-stream_handler = logging.StreamHandler()
-
-# handlerのログレベル設定(ハンドラが出力するエラーメッセージのレベル)
-stream_handler.setLevel(level)
-
-# ログ出力フォーマット設定
-handler_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # noqa: E501
-stream_handler.setFormatter(handler_format)
-
-# --------------------------------
-# 3.loggerにhandlerをセット
-# --------------------------------
-logger.addHandler(stream_handler)
-
-# --------------------------------
-# ログ出力テスト
-# --------------------------------
+class Color:
+    BLACK          = '\033[30m'  # (文字)黒
+    RED            = '\033[31m'  # (文字)赤
+    GREEN          = '\033[32m'  # (文字)緑
+    YELLOW         = '\033[33m'  # (文字)黄
+    BLUE           = '\033[34m'  # (文字)青
+    MAGENTA        = '\033[35m'  # (文字)マゼンタ
+    CYAN           = '\033[36m'  # (文字)シアン
+    WHITE          = '\033[37m'  # (文字)白
+    COLOR_DEFAULT  = '\033[39m'  # 文字色をデフォルトに戻す
+    BOLD           = '\033[1m'   # 太字
+    UNDERLINE      = '\033[4m'   # 下線
+    INVISIBLE      = '\033[08m'  # 不可視
+    REVERCE        = '\033[07m'  # 文字色と背景色を反転
+    BG_BLACK       = '\033[40m'  # (背景)黒
+    BG_RED         = '\033[41m'  # (背景)赤
+    BG_GREEN       = '\033[42m'  # (背景)緑
+    BG_YELLOW      = '\033[43m'  # (背景)黄
+    BG_BLUE        = '\033[44m'  # (背景)青
+    BG_MAGENTA     = '\033[45m'  # (背景)マゼンタ
+    BG_CYAN        = '\033[46m'  # (背景)シアン
+    BG_WHITE       = '\033[47m'  # (背景)白
+    BG_DEFAULT     = '\033[49m'  # 背景色をデフォルトに戻す
+    RESET          = '\033[0m'   # 全てリセット
 
 
 def main(args):
@@ -89,10 +114,10 @@ def main(args):
     pr = repository.pullrequests.get(bitbucket_pr_id)
 
     # logger.debug(vars(pr))
-    logger.info(f'{pr.title=}')
-    logger.info(f'{pr.description=}')
-    logger.info(f'{pr.diff()=}')
-    logger.info(f'{pr.diffstat()=}')
+    # logger.info(f'{pr.title=}')
+    # logger.info(f'{pr.description=}')
+    # logger.info(f'{pr.diff()=}')
+    # logger.info(f'{pr.diffstat()=}')
 
     responses = []
 
@@ -116,12 +141,8 @@ def main(args):
         if diff.strip():
             file_diffs.append(f'diff --git{diff}')
 
-    print()
-    print()
-    print()
-    print()
     for index, diffstat in enumerate(pr.diffstat()):
-        print('8' * 100)
+        # print('8' * 100)
         # print('diffstat')
         # print(diffstat)
         # pprint(diffstat.new.__dir__())
@@ -139,34 +160,25 @@ def main(args):
         prompt.file_diff = file_diff
         # print('WE ARE SAPPORO!')
         messages[1]['content'] = f'{prompt.summarize_file_diff}'
-        print('************* messages **********')
-        print(messages[1]['content'])
+        # print('************* messages **********')
+        # print(messages[1]['content'])
         # logger.info(f'{messages=}')
 
         response = completion(model=AI_MODEL, messages=messages)
         content = response.get('choices', [{}])[0].get('message', {}).get('content')
-        print('************* response **********')
-        print(response)
-        prompt.short_summary = content
-        messages[1]['content'] = f'{prompt.review_file_diff}\nin Japanese'
+        # print('************* response **********')
+        # print(content)
+        prompt.short_summary = content.split('## Triage:')[0]
+        language = 'Please your answer description in Japanese.'
+        messages[1]['content'] = f'{language}\n{prompt.review_file_diff}'
         print('************* messages **********')
-        print(messages[1]['content'])
+        print(f'{Color.RED}{messages[1]["content"]}{Color.RESET}')
         # logger.info(f'{messages=}')
         response = completion(model=AI_MODEL, messages=messages)
-        print('************* response **********')
-        print(response)
         content = response.get('choices', [{}])[0].get('message', {}).get('content')
+        print('************* response **********')
+        print(f'{Color.GREEN}{content}{Color.RESET}')
         responses.append(content)
-
-
-    print()
-    print()
-    print()
-    print(responses)
-    print()
-    for res in responses:
-        print(res)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='')
--- prompt.py
+++ prompt.py
@@ -11,38 +11,381 @@ class BasePrompt:
     _REVIEW_FILE_DIFF = ''
     _COMMENT = ''
 
-    def __format(self, template):
+    def _format(self, template):
         replaces = {key: value for key, value in self.__dict__.items()}
         return template.format(**replaces)
         # return '\n'.join([eval(f"f'{row}'") for row in template.split('\n')])
 
     @property
     def summarize_file_diff(self):
-        return self.__format(self._SUMMARIZE_FILE_DIFF + self._TRIAGE_FILE_DIFF)  # noqa: E501
+        return self._format(self._SUMMARIZE_FILE_DIFF + self._TRIAGE_FILE_DIFF)  # noqa: E501
 
     @property
     def summarize_changesets(self):
-        return self.__format(self._SUMMARIZE_CHANGESETS)
+        return self._format(self._SUMMARIZE_CHANGESETS)
 
     @property
     def summarize(self):
-        return self.__format(self._SUMMARIZE_PREFIX + self._SUMMARIZE)
+        return self._format(self._SUMMARIZE_PREFIX + self._SUMMARIZE)
 
     @property
     def summarize_short(self):
-        return self.__format(self._SUMMARIZE_PREFIX + self._SUMMARIZE_SHORT)
+        return self._format(self._SUMMARIZE_PREFIX + self._SUMMARIZE_SHORT)
 
     @property
     def summarize_release_notes(self):
-        return self.__format(self._SUMMARIZE_PREFIX + self._SUMMARIZE_RELEASE_NOTES)  # noqa: E501
+        return self._format(self._SUMMARIZE_PREFIX + self._SUMMARIZE_RELEASE_NOTES)  # noqa: E501
 
     @property
     def review_file_diff(self):
-        return self.__format(self._REVIEW_FILE_DIFF)
+        return self._format(self._REVIEW_FILE_DIFF)
 
     @property
     def comment(self):
-        return self.__format(self._COMMENT)
+        return self._format(self._COMMENT)
+
+
+class MyPrompt(BasePrompt):
+    # PRタイトル
+    title = ''
+    # PR説明
+    description = ''
+    # ファイルの差分
+    file_diff = ''
+    # ファイル変更の要約
+    raw_summary = ''
+    # 変更の要約
+    short_summary = ''
+    patches = ''
+    diff = ''
+    comment_chain = ''
+    comment = ''
+    filename = ''
+
+    # プロンプトの説明
+    _SYSTEM_MESSAGE = {
+        'description': 'System message to be sent to OpenAI',
+        'default': '''
+You are `@coderabbitai` (aka `github-actions[bot]`), a language model trained by OpenAI. Your purpose is to act as a highly experienced software engineer and provide a thorough review of the code hunks and suggest code snippets to improve key areas such as:
+  - Logic
+  - Security
+  - Performance
+  - Data races
+  - Consistency
+  - Error handling
+  - Maintainability
+  - Modularity
+  - Complexity
+  - Optimization
+  - Best practices: DRY, SOLID, KISS
+
+Do not comment on minor code style issues, missing comments/documentation. Identify and resolve significant concerns to improve overall code quality while deliberately disregarding minor issues.
+        '''  # noqa[E501],
+    }
+
+    # 全体の要約生成プロンプト
+    _SUMMARIZE_PROMOPT_PROVIDE = {
+        'description': 'The prompt for final summarization response',
+        'default': '''
+Provide your final response in markdown with the following content:
+
+- **Walkthrough**: A high-level summary of the overall change instead of specific files within 80 words.
+- **Changes**: A markdown table of files and their summaries. Group files with similar changes together into a single row to save space.
+- **Poem**: Below the changes, include a whimsical, short poem written by a rabbit to celebrate the changes. Format the poem as a quote using the ">" symbol and feel free to use emojis where relevant.
+
+Avoid additional commentary as this summary will be added as a comment on the GitHub pull request. Use the titles "Walkthrough" and "Changes" and they must be H2.
+        ''',  # noqa[E501]
+    }
+
+    # リリースノートの要約生成プロンプト
+    _SUMMARIZE_RELEASE_NOTES_PROVIDE = {
+        'description': 'The prompt for generating release notes in the same chat as summarize stage',  # noqa[E501]
+        'default': '''
+Craft concise release notes for the pull request.
+Focus on the purpose and user impact, categorizing changes as "New Feature", "Bug Fix",
+"Documentation", "Refactor", "Style", "Test", "Chore", or "Revert". Provide a bullet-point list,
+e.g., "- New Feature: Added search functionality to the UI". Limit your response to 50-100 words
+and emphasize features visible to the end-user while omitting code-level details.
+        '''  # noqa[E501],
+    }
+
+    _SUMMARIZE_CHANGESETS = '''
+Provided below are changesets in this pull request. Changesets are in chronlogical order and new changesets are appended to the end of the list. The format consists of filename(s) and the summary of changes for those files. There is a separator between each changeset.
+Your task is to deduplicate and group together files with related/similar changes into a single changeset. Respond with the updated changesets using the same format as the input.
+
+{raw_summary}
+''' # noqa[E501]
+
+    _SUMMARIZE_SHORT = '''
+Your task is to provide a concise summary of the changes. This summary will be used as a prompt while reviewing each file and must be very clear for the AI bot to understand.
+
+Instructions:
+
+- Focus on summarizing only the changes in the PR and stick to the facts.
+- Do not provide any instructions to the bot on how to perform the review.
+- Do not mention that files need a through review or caution about potential issues.
+- Do not mention that these changes affect the logic or functionality of the code.
+- The summary should not exceed 500 words.
+''' # noqa[E501]
+
+    # 差分の要約生成プロンプト
+    _SUMMARIZE_FILE_DIFF = '''
+## GitHub PR Title
+
+{title}
+
+## Description
+
+{description}
+
+## Diff
+
+### Example Pull Request Diff
+
+``` diff
+## file: 'src/file1.py'
+
+@@ -12,5 +12,5 @@ def func1():
+code line 1 that remained unchanged in the PR
+code line 2 that remained unchanged in the PR
+-code line that was removed in the PR
++code line added in the PR
+code line 3 that remained unchanged in the PR
+
+@@ ... @@ def func2():
+...
+
+
+## file: 'src/file2.py'
+...
+```
+
+### The Pull Request Diff
+
+```diff
+{file_diff}
+```
+
+## Instructions
+
+I would like you to succinctly summarize the diff within 100 words.
+If applicable, your summary should include a note about alterations
+to the signatures of exported functions, global data structures and
+variables, and any changes that might affect the external interface or
+behavior of the code.
+''' # noqa[E501]
+
+    # ファイルの変更要約生成プロンプト
+    _TRIAGE_FILE_DIFF = '''
+Below the summary, I would also like you to triage the diff as `NEEDS_REVIEW` or `APPROVED` based on the following criteria:
+
+- If the diff involves any modifications to the logic or functionality, even if they seem minor, triage it as `NEEDS_REVIEW`. This includes changes to control structures, function calls, or variable assignments that might impact the behavior of the code.
+- If the diff only contains very minor changes that don't affect the code logic, such as fixing typos, formatting, or renaming variables for clarity, triage it as `APPROVED`.
+
+Please evaluate the diff thoroughly and take into account factors such as the number of lines changed, the potential impact on the overall system, and the likelihood of introducing new bugs or security vulnerabilities.
+When in doubt, always err on the side of caution and triage the diff as `NEEDS_REVIEW`.
+
+You must strictly follow the format below for triaging the diff:
+[TRIAGE]: <NEEDS_REVIEW or APPROVED>
+
+Important:
+- In your summary do not mention that the file needs a through review or caution about
+  potential issues.
+- Do not provide any reasoning why you triaged the diff as `NEEDS_REVIEW` or `APPROVED`.
+- Do not mention that these changes affect the logic or functionality of the code in the summary. You must only use the triage status format above to indicate that.
+''' # noqa[E501]
+
+    _SUMMARIZE_PREFIX = '''
+Here is the summary of changes you have generated for files:
+```
+{raw_summary}
+```
+    '''
+
+    # ファイルの変更のレビュー要求
+    _REVIEW_FILE_DIFF = '''
+## GitHub PR Title
+
+`{title}`
+
+## Description
+
+```
+{description}
+```
+
+## Summary of changes
+
+```
+{short_summary}
+```
+
+## IMPORTANT Instructions
+
+Input: New hunks annotated with line numbers and old hunks (replaced code). Hunks represent incomplete code fragments.
+Additional Context: PR title, description, summaries and comment chains.
+Task: Review new hunks for substantive issues using provided context and respond with comments if necessary.
+Output: Review comments in markdown with exact line number ranges in new hunks. Start and end line numbers must be within the same hunk. For single-line comments, start=end line number. Must use example response format below.
+Use fenced code blocks using the relevant language identifier where applicable.
+Don't annotate code snippets with line numbers. Format and indent code correctly.
+Do not use `suggestion` code blocks.
+For fixes, use `diff` code blocks, marking changes with `+` or `-`. The line number range for comments with fix snippets must exactly match the range to replace in the new hunk.
+
+- Do NOT provide general feedback, summaries, explanations of changes, or praises
+  for making good additions.
+- Focus solely on offering specific, objective insights based on the
+  given context and refrain from making broad comments about potential impacts on
+  the system or question intentions behind the changes.
+
+If there are no issues found on a line range, you MUST respond with the
+text `LGTM!` for that line range in the review section.
+
+## Example
+
+### Example changes
+
+---new_hunk---
+```
+  z = x / y
+    return z
+
+20: def add(x, y):
+21:     z = x + y
+22:     retrn z
+23:
+24: def multiply(x, y):
+25:     return x * y
+
+def subtract(x, y):
+  z = x - y
+```
+
+---old_hunk---
+```
+  z = x / y
+    return z
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    z = x - y
+```
+
+---comment_chains---
+```
+Please review this change.
+```
+
+---end_change_section---
+
+### Example response
+
+22-22:
+There's a syntax error in the add function.
+```diff
+-    retrn z
++    return z
+```
+---
+24-25:
+LGTM!
+---
+
+## Changes made to `{filename}` for your review
+
+''' # noqa[E501]
+
+    _COMMENT = '''
+A comment was made on a GitHub PR review for a
+diff hunk on a file - `{filename}`. I would like you to follow
+the instructions in that comment.
+
+## GitHub PR Title
+
+`{title}`
+
+## Description
+
+```
+{description}
+```
+
+## Summary generated by the AI bot
+
+```
+{short_summary}
+```
+
+## Entire diff
+
+```diff
+{file_diff}
+```
+
+## Diff being commented on
+
+```diff
+{diff}
+```
+
+## Instructions
+
+Please reply directly to the new comment (instead of suggesting a reply) and your reply will be posted as-is.
+
+If the comment contains instructions/requests for you, please comply.
+For example, if the comment is asking you to generate documentation comments on the code, in your reply please generate the required code.
+
+In your reply, please make sure to begin the reply by tagging the user with "@user".
+
+## Comment format
+
+`user: comment`
+
+## Comment chain (including the new comment)
+
+```
+{comment_chain}
+```
+
+## The comment/request that you need to directly reply to
+
+```
+{comment}
+```
+''' # noqa[E501]
+
+    _GIT_DIFF = '''
+### Example Pull Request Diff
+
+``` diff
+## file: 'src/file1.py'
+
+@@ -12,5 +12,5 @@ def func1():
+code line 1 that remained unchanged in the PR
+code line 2 that remained unchanged in the PR
+-code line that was removed in the PR
++code line added in the PR
+code line 3 that remained unchanged in the PR
+
+@@ ... @@ def func2():
+...
+
+
+## file: 'src/file2.py'
+...
+```
+
+### The Pull Request Diff
+
+```diff
+{file_diff}
+```
+'''
+
+    @property
+    def review_file_diff(self):
+        return self._format(self._REVIEW_FILE_DIFF + self._GIT_DIFF)
 
 
 class CodeRabbitPrompt(BasePrompt):
